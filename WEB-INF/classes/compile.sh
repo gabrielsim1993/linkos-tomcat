def17b64b118c8e57eb6fc23fb7f458c00f94417
Justin <njyjustin@gmail.com>
2017-11-02 20:02:00 -0700

    Modify compile.sh script to compile all java files and dependencies

--- WEB-INF/classes/compile.sh
+++ WEB-INF/classes/compile.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-javac -classpath .:/usr/local/Cellar/tomcat/8.5.23/libexec/lib/servlet-api.jar:/usr/local/Cellar/tomcat/8.5.23/libexec/webapps/workato/WEB-INF/lib/ZSDK_API.jar:/usr/local/Cellar/tomcat/8.5.23/libexec/lib/gson-2.6.2.jar HomeServlet.java
+#!/usr/bin/env bash
+echo Compiling classes...
+javac -classpath .:/usr/local/Cellar/tomcat/8.5.23/libexec/lib/*:/usr/local/Cellar/tomcat/8.5.23/libexec/webapps/workato/WEB-INF/lib/ZSDK_API.jar *.java
+echo Done!
